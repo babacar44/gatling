492c044581d1c243777953472e4c321a88cadb23
Babacar44 <babacamara91@gmail.com>
2023-12-09 12:37:33 +0100

    jsonPath check response

--- GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -6,9 +6,9 @@ import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class VideoGameDb extends Simulation {
 
@@ -22,15 +22,19 @@ public class VideoGameDb extends Simulation {
     // 2 Scenario Definition
     private ScenarioBuilder scn = scenario("Video Game Db - section 5 code")
             .exec(http(GET_ALL_VIDEOS_GAMES + "- 1st call")
-                    .get(VIDEOGAME))
+                    .get(VIDEOGAME)
+                    .check(status().is(200))
+                    .check(jsonPath("$[?(@.id==1)].name").is("Resident Evil 4")))
             .pause(5)
 
             .exec(http("Get specific game")
-                    .get("/videogame/1"))
+                    .get("/videogame/1")
+                    .check(status().in(200, 201,202)))
             .pause(1,10)
             
             .exec(http(GET_ALL_VIDEOS_GAMES + "- 2nd call")
-                    .get(VIDEOGAME))
+                    .get(VIDEOGAME)
+                    .check(status().not(404),status().not(500)))
             .pause(Duration.ofMillis(4000));
 
     // 3 Load Simulation
