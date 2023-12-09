5b4334f059609ebb5e5bd79a468f53344cfecd8b
Babacar44 <babacamara91@gmail.com>
2023-12-09 13:25:43 +0100

    debug

--- GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -17,6 +17,7 @@ public class VideoGameDb extends Simulation {
     public static final String GET_ALL_VIDEOS_GAMES = "Get all videos games ";
     public static final String GET_SPECIFIC_GAME = "Get specific game";
     public static final String GAME_ID = "gameId";
+    public static final String RESPONSE_BODY = "responseBody";
     // 1 Http Configuration
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://videogamedb.uk/api")
@@ -36,10 +37,24 @@ public class VideoGameDb extends Simulation {
                     .check(status().not(404),status().not(500))
                     .check(jmesPath("[1].id").saveAs(GAME_ID)))
             .pause(Duration.ofMillis(4000))
+            .exec(
+                    session -> {
+                        System.out.println(session);
+                        System.out.println("gameId set to: "+ session.getString(GAME_ID));
+                        return session;
+                    }
+            )
 
             .exec(http(GET_SPECIFIC_GAME +" with Id - #{"+GAME_ID+"}")
                     .get("/videogame/#{"+GAME_ID+"}")
-                    .check(jmesPath("name").is("Gran Turismo 3")));
+                    .check(jmesPath("name").is("Gran Turismo 3"))
+                    .check(bodyString().saveAs(RESPONSE_BODY)))
+            .exec(
+                    session -> {
+                        System.out.println("Response Body: " + session.getString(RESPONSE_BODY));
+                        return session;
+                    }
+            );
 
 
 
