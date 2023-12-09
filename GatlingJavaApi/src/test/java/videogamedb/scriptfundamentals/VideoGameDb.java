f3ec9b22898913f731eaea0dc12dd58b2666b551
Babacar44 <babacamara91@gmail.com>
2023-12-09 14:27:59 +0100

    Looping HTTP Calls

--- GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -20,21 +20,27 @@ public class VideoGameDb extends Simulation {
             .acceptHeader("application/json");
 
     private static final ChainBuilder getAllVideoGames =
-            exec(http(GET_ALL_VIDEOS_GAMES)
-                    .get(VIDEO_GAME_ENDPT)
-                    .check(status().not(404), status().not(500)));
+            repeat(3).on(
+                    exec(http(GET_ALL_VIDEOS_GAMES)
+                            .get(VIDEO_GAME_ENDPT)
+                            .check(status().not(404), status().not(500)))
+            );
 
     private static final ChainBuilder getSpecificVideoGame =
-            exec(http(GET_SPECIFIC_GAME)
-                    .get(VIDEO_GAME_ENDPT + "/1")
-                    .check(status().is(200)));
+            repeat(5, "myCounter").on(
+                    exec(http(GET_SPECIFIC_GAME+ "with id: #{myCounter}")
+                            .get(VIDEO_GAME_ENDPT + "/#{myCounter}")
+                            .check(status().is(200)))
+            );
     // 2 Scenario Definition
     private final ScenarioBuilder scn = scenario("Video Game Db - section 5 code")
             .exec(getAllVideoGames)
             .pause(5)
             .exec(getSpecificVideoGame)
             .pause(5)
-            .exec(getAllVideoGames);
+            .repeat(2).on(
+                    exec(getAllVideoGames)
+            );
 
     // 3 Load Simulation
     {
