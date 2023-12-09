93a1d1b175cb40b2e1763577119723d940d93970
Babacar44 <babacamara91@gmail.com>
2023-12-09 14:09:05 +0100

    refacto into methods

--- GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ GatlingJavaApi/src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -1,5 +1,6 @@
 package videogamedb.scriptfundamentals;
 
+import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
@@ -13,50 +14,32 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class VideoGameDb extends Simulation {
 
-    public static final String VIDEOGAME = "/videogame";
     public static final String GET_ALL_VIDEOS_GAMES = "Get all videos games ";
     public static final String GET_SPECIFIC_GAME = "Get specific game";
     public static final String GAME_ID = "gameId";
     public static final String RESPONSE_BODY = "responseBody";
+    public static final String VIDEO_GAME_ENDPT = "/videogame";
     // 1 Http Configuration
-    private HttpProtocolBuilder httpProtocol = http
+    private final HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://videogamedb.uk/api")
             .acceptHeader("application/json");
 
+    private static final ChainBuilder getAllVideoGames =
+            exec(http(GET_ALL_VIDEOS_GAMES)
+                    .get(VIDEO_GAME_ENDPT)
+                    .check(status().not(404), status().not(500)));
+
+    private static final ChainBuilder getSpecificVideoGame =
+            exec(http(GET_SPECIFIC_GAME)
+                    .get(VIDEO_GAME_ENDPT + "/1")
+                    .check(status().is(200)));
     // 2 Scenario Definition
-    private ScenarioBuilder scn = scenario("Video Game Db - section 5 code")
-
-            .exec(http(GET_SPECIFIC_GAME)
-                    .get("/videogame/1")
-                    .check(status().in(200, 201,202))
-                    .check(jmesPath("name").is("Resident Evil 4")))
-            .pause(1,10)
-            
-            .exec(http(GET_ALL_VIDEOS_GAMES)
-                    .get(VIDEOGAME)
-                    .check(status().not(404),status().not(500))
-                    .check(jmesPath("[1].id").saveAs(GAME_ID)))
-            .pause(Duration.ofMillis(4000))
-            .exec(
-                    session -> {
-                        System.out.println(session);
-                        System.out.println("gameId set to: "+ session.getString(GAME_ID));
-                        return session;
-                    }
-            )
-
-            .exec(http(GET_SPECIFIC_GAME +" with Id - #{"+GAME_ID+"}")
-                    .get("/videogame/#{"+GAME_ID+"}")
-                    .check(jmesPath("name").is("Gran Turismo 3"))
-                    .check(bodyString().saveAs(RESPONSE_BODY)))
-            .exec(
-                    session -> {
-                        System.out.println("Response Body: " + session.getString(RESPONSE_BODY));
-                        return session;
-                    }
-            );
-
-
+    private final ScenarioBuilder scn = scenario("Video Game Db - section 5 code")
+            .exec(getAllVideoGames)
+            .pause(5)
+            .exec(getSpecificVideoGame)
+            .pause(5)
+            .exec(getAllVideoGames);
 
     // 3 Load Simulation
     {
